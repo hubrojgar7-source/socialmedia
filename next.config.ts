5c6f2f8288986cc2b2e5b7e08d747308e011da0d
hubrojgar7-source <hubrojgar7@gmail.com>
2026-06-29 22:46:29 +0545

    Fix next/image hostname: add ik.imagekit.io, graph.facebook.com, ui-avatars.com to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "graph.facebook.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
